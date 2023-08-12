58f2863ee8d74321e1ffacb2b6a040c3dd5e3d6b
Shweta Rajendra Saindane <80962110+shweta27-alt@users.noreply.github.com>
2023-08-12 23:05:34 +0530

    home page added

---
+++ src/components/Home.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import vg from "../assests/2.webp";
+import {AiFillGoogleCircle, AiFillAmazonCircle, AiFillYoutube, AiFillInstagram} from "react-icons/ai"
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <main>
+          <h1>Techystar</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse At vero eos et accusamus et
+            iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+            deleniti atque corrupti quos dolores et quas molestias excepturi
+            sint occaecati cupiditate non provident, similique sunt in culpa qui
+            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et
+            harum quidem rerum facilis est et expedita distinctio.
+          </p>
+        </div>
+      </div>
+
+      <div className="home4">
+        <div>
+            <h1>Brands</h1>
+            <article>
+              <div style={{animationDelay: "0.3s"}}>
+                <AiFillGoogleCircle/>
+                <p>Google</p>
+              </div>
+
+              <div style={{animationDelay: "0.5s"}}>
+                <AiFillAmazonCircle/>
+                <p>Amazon</p>
+              </div>
+
+              <div style={{animationDelay: "0.7s"}}>
+                <AiFillYoutube/>
+                <p>Youtube</p>
+              </div>
+
+              <div style={{animationDelay: "0.9s"}}>
+                <AiFillInstagram/>
+                <p>Instagram</p>
+              </div>
+            </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
